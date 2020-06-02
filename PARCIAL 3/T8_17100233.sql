2eceb0a44180789ec391a400ed90afa918ce06fc
Maro Hdz <marohdzg@gmail.com>
2020-06-02 13:51:11 -0500

    Nuevo script

--- PARCIAL 3/T8_17100233.sql
+++ PARCIAL 3/T8_17100233.sql
@@ -16,49 +16,31 @@
 CREATE DATABASE IF NOT EXISTS `fgbdd` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci */;
 USE `fgbdd`;
 
--- Volcando estructura para tabla fgbdd.usuario
-CREATE TABLE IF NOT EXISTS `usuario` (
+-- Volcando estructura para tabla fgbdd.proveedor
+CREATE TABLE IF NOT EXISTS `proveedor` (
+  `CLAVEPROV` smallint(6) NOT NULL AUTO_INCREMENT,
+  `NOMBRE` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `APELLIDOP` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `APELLIDOM` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `NUMTEL` bigint(20) DEFAULT NULL,
   `CORREO` varchar(70) COLLATE utf8_spanish_ci NOT NULL,
-  `CONTRASENA` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
-  `ROL` varchar(1) COLLATE utf8_spanish_ci NOT NULL DEFAULT 'C',
-  PRIMARY KEY (`CORREO`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+  `ROL` varchar(1) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`CLAVEPROV`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
--- Volcando datos para la tabla fgbdd.usuario: ~30 rows (aproximadamente)
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` (`CORREO`, `CONTRASENA`, `ROL`) VALUES
-	('beto_porras@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'P'),
-	('carlos_moon@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'E'),
-	('cod_glzhdz@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'A'),
-	('fco_camero@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'A'),
-	('gabo_pdrz@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'C'),
-	('gera_arevalo@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'P'),
-	('hugo_hdz@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'E'),
-	('isma_glz@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'C'),
-	('isra_camero@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'P'),
-	('ivan_jrz@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'P'),
-	('jose_jose@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'P'),
-	('jose_vela@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'E'),
-	('juan_jrz@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'P'),
-	('juan_soto@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'C'),
-	('karla_hurtado@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'E'),
-	('lalo_segovia@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'E'),
-	('luis_miguel@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'P'),
-	('mago_card@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'C'),
-	('marohdzg@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'A'),
-	('memo_vazquez@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'P'),
-	('misa_gtz@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'E'),
-	('omar_ortiz@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'C'),
-	('Orlando_lpz@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'E'),
-	('pedro_garza@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'C'),
-	('pepe_wong@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'C'),
-	('poncho_glz@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'E'),
-	('rick_gmz@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'C'),
-	('roberto_landeros@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'C'),
-	('tavo@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'P'),
-	('tomas_carpedr@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'E'),
-	('vieyra_beto@codeux.com', '*84AAC12F54AB666ECFC2A83C676908C8BBC381B1', 'P');
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+-- Volcando datos para la tabla fgbdd.proveedor: ~9 rows (aproximadamente)
+/*!40000 ALTER TABLE `proveedor` DISABLE KEYS */;
+INSERT INTO `proveedor` (`CLAVEPROV`, `NOMBRE`, `APELLIDOP`, `APELLIDOM`, `NUMTEL`, `CORREO`, `ROL`) VALUES
+	(2, 'Roberto', 'Vieyra', 'López', 86793020, 'vieyra_beto@codeux.com', 'P'),
+	(3, 'Gustavo', 'Martínez', 'Lopez', 811399200, 'tavo@codeux.com', 'P'),
+	(4, 'Gerardo', 'Ríos', 'Arevalo', 812399920, 'gera_arevalo@codeux.com', 'P'),
+	(5, 'Israel', 'González', 'Camero', 812939203, 'isra_camero@codeux.com', 'P'),
+	(6, 'Guillermo', 'Vázquez', 'Gallardo', 833849390, 'memo_vazquez@codeux.com', 'P'),
+	(7, 'Luis', 'Gallego', 'Basteri', 8881929300, 'comodishe@codeux.com', 'P'),
+	(8, 'José', 'Sosa', 'Ortíz', 8729039200, 'jose_jose@codeux.com', 'P'),
+	(9, 'Juan', 'Baltazar', 'Juarez', 8729039200, 'juan_jrz@codeux.com', 'P'),
+	(10, 'Iván', 'Alfaro', 'Juarez', 872300290, 'ivan_jrz@codeux.com', 'P');
+/*!40000 ALTER TABLE `proveedor` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
